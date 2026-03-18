616cf8a7de568e7310dff355da44fe75e5926298
hponethit04 <113315069+hponethit04@users.noreply.github.com>
2026-03-17 20:44:22 -0400

    added comments

--- sensitivity_analysis.m
+++ sensitivity_analysis.m
@@ -1,13 +1,13 @@
 function sensitivity_analysis(T)
 
     % Model Specification Sensitivity
- 
+    % different models to see how coefficients changes 
     models = {
-        'LogAmount ~ Age + Severity + Attorney + Specialty'; % base regression
-        'LogAmount ~ Age + Severity + Attorney + Specialty + InsuranceType'; % +insurance
-        'LogAmount ~ Age + Severity + Attorney + Specialty + Gender', '+ Gender'; % +gender
-        'LogAmount ~ Age + Severity + Attorney + Specialty + MaritalStatus'; %+marital status
-        'LogAmount ~ Age + Severity + Attorney + Specialty + InsuranceType + Gender + MaritalStatus'; %all
+        'LogAmount ~ Age + Severity + Attorney + Specialty'; 
+        'LogAmount ~ Age + Severity + Attorney + Specialty + InsuranceType'; 
+        'LogAmount ~ Age + Severity + Attorney + Specialty + Gender';
+        'LogAmount ~ Age + Severity + Attorney + Specialty + MaritalStatus'; 
+        'LogAmount ~ Age + Severity + Attorney + Specialty + InsuranceType + Gender + MaritalStatus'; 
     };
 
     for i = 1:height(models)
@@ -16,8 +16,10 @@ function sensitivity_analysis(T)
     end
 
     % Outlier Sensitivity 
+    % removes potential outliers to analyze how coefficients change
 
     thresholds = [0.99, 0.97, 0.95];   % remove top 1%, 3%, 5%
+    labels = {'Top 1% removed', 'Top 3% removed', 'Top 5% removed'};
     base_mdl   = fitlm(T, 'LogAmount ~ Age + Severity + Attorney + Specialty');
     disp(base_mdl.Coefficients);
 
@@ -25,6 +27,7 @@ function sensitivity_analysis(T)
         cutoff   = quantile(T.LogAmount, thresholds(i));
         T_trim   = T(T.LogAmount <= cutoff, :);
         mdl_trim = fitlm(T_trim, 'LogAmount ~ Age + Severity + Attorney + Specialty');
+        fprintf('\n%s (n=%d)\n', labels{i}, height(T_trim));
         disp(mdl_trim.Coefficients);
     end
 
